9b5361437cbd1120ef7e0c55f25fa73c525d9b95
Cameron Tong <cameron.tong2b@gmail.com>
2021-06-17 21:44:07 -0400

    Implement adding and displaying tasks

--- day_planner.rb
+++ day_planner.rb
@@ -1,1 +1,16 @@
-p 3 + 3
+# add items to specific time slots
+# be alerted if items overlap in time
+# be prompted at the end of day planner item on compleition status
+system "clear"
+
+tasks = []
+loop do
+  print "Enter Task Here => "
+  input = gets
+  tasks << input
+  puts
+  puts "-- Tasks --"
+  puts tasks
+  puts
+end
+
