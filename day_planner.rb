875cb3917522cb7bc750eac62a9f3762a5d102b5
Cameron Tong <cameron.tong2b@gmail.com>
2021-06-22 22:02:32 -0400

    Made tasks persistent

--- day_planner.rb
+++ day_planner.rb
@@ -1,16 +1,26 @@
 # add items to specific time slots
+# add associated start and end times
+# storage of tasks
 # be alerted if items overlap in time
 # be prompted at the end of day planner item on compleition status
 system "clear"
+require "yaml/store"
 
-tasks = []
+store = YAML::Store.new "./task_list.yml"
+tasks = nil
+store.transaction do
+  tasks = store["tasks"]
+end
+
 loop do
   print "Enter Task Here => "
   input = gets
   tasks << input
+  store.transaction do
+    store["tasks"] = tasks
+  end
   puts
   puts "-- Tasks --"
   puts tasks
   puts
 end
-
