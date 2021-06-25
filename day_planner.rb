c9a8001c04e031f4fc3d516ebc97a4fc4a825a25
Cameron Tong <cameron.tong2b@gmail.com>
2021-06-24 21:48:54 -0400

    display tasks when program starts

--- day_planner.rb
+++ day_planner.rb
@@ -3,24 +3,25 @@
 # storage of tasks
 # be alerted if items overlap in time
 # be prompted at the end of day planner item on compleition status
-system "clear"
 require "yaml/store"
 
-store = YAML::Store.new "./task_list.yml"
+system "clear"
+
+store = YAML::Store.new("./task_list.yml")
 tasks = nil
 store.transaction do
   tasks = store["tasks"]
 end
 
 loop do
+  puts "-- Tasks --"
+  puts tasks
+  puts
   print "Enter Task Here => "
   input = gets
   tasks << input
   store.transaction do
     store["tasks"] = tasks
   end
-  puts
-  puts "-- Tasks --"
-  puts tasks
   puts
 end
