0745f7a407fa43c553f022c5ccf06cda911db441
Cameron Tong <cameron.tong2b@gmail.com>
2021-07-06 21:30:40 -0400

    refactor: add class and 'main' function

--- day_planner.rb
+++ day_planner.rb
@@ -4,57 +4,63 @@
 # be prompted at the end of day planner item on compleition status
 require "yaml/store"
 
-system "clear"
+class DayPlanner
+  TEST_INPUTS = ["12:00 13:00 lift weights"]
 
-TEST_INPUTS = ["12:00 13:00 lift weights"]
+  def format_task(task)
+    parsed_task = task.split
+    start_time, end_time, *task_description = parsed_task
+    [start_time, end_time, task_description].join(" ")
+  end
 
-def format_task(task)
-  parsed_task = task.split
-  start_time, end_time, *task_description = parsed_task
-  [start_time, end_time, task_description].join(" ")
-end
+  def get_string
+    if test_mode?
+      test_input
+    else
+      gets
+    end
+  end
 
-def test_mode?
-  true
-end
+  def test_mode?
+    true
+  end
 
-def get_string
-  if test_mode?
-    test_input
-  else
-    gets
+  def test_input
+    TEST_INPUTS.shift || exit
+  end
+
+  def build_store
+    if test_mode?
+      YAML::Store.new("./task_list_test.yml")
+    else
+      YAML::Store.new("./task_list.yml")
+    end
+  end
+
+  def main
+    system "clear"
+    store = build_store
+    tasks = nil
+    store.transaction do
+      tasks = store["tasks"] || []
+    end
+
+    loop do
+      puts "-- Tasks --"
+      puts tasks.map(&method(:format_task))
+      puts
+      print "Enter Task Here => "
+      input = get_string
+      tasks << input
+      store.transaction do
+        store["tasks"] = tasks
+      end
+      store.transaction { store["tasks"] = [] } if TEST_INPUTS.empty?
+
+      puts
+    end
   end
 end
 
-def test_input
-  TEST_INPUTS.shift || exit
-end
-
-def build_store
-  if test_mode?
-    YAML::Store.new("./task_list_test.yml")
-  else
-    YAML::Store.new("./task_list.yml")
-  end
-end
-
-store = build_store
-tasks = nil
-store.transaction do
-  tasks = store["tasks"] || []
-end
-
-loop do
-  puts "-- Tasks --"
-  puts tasks.map(&method(:format_task))
-  puts
-  print "Enter Task Here => "
-  input = get_string
-  tasks << input
-  store.transaction do
-    store["tasks"] = tasks
-  end
-  store.transaction { store["tasks"] = [] } if TEST_INPUTS.empty?
-
-  puts
-end
+task_test = DayPlanner.new
+task_test.main
