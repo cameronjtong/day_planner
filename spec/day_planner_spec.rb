32b0a4d44a2eccaed1755bfac951d9944144f9b8
Cameron Tong <cameron.tong2b@gmail.com>
2021-08-18 11:18:45 -0400

    test: refactor tests

--- spec/day_planner_spec.rb
+++ spec/day_planner_spec.rb
@@ -2,14 +2,27 @@ require "rspec"
 require_relative "../day_planner"
 
 ENV["DAY_PLANNER_FILENAME"] = "./task_list_test.yml"
+FILENAME = ENV["DAY_PLANNER_FILENAME"]
 
 describe "a day planner" do
+  before { system "rm #{FILENAME}" }
+
   it "creates lists" do
-    test_inputs = ["Grocery List", "apples", "bread", "-2", "Reading List", "^ ", "v ", "v ", " ", "-"]
+    test_inputs = ["Grocery List"]
+
+    process_inputs(test_inputs)
+
+    expect(lists.list_names.first).to eq("Grocery List")
+  end
+
+  def process_inputs(test_inputs)
     day_planner = DayPlanner.new(test_inputs)
     expect { day_planner.main }.to output(//).to_stdout
-    TaskRepository.persist do |lists|
-      expect(lists.list_names.first).to eq("Reading List")
+  end
+
+  def lists
+    TaskRepository.persist do |task_lists|
+      return task_lists
     end
   end
 end
